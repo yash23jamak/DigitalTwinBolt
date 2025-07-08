97dc60a5e8a47706a15b4570c22e692edc58fa6b
yash23jamak <129269943+yash23jamak@users.noreply.github.com>
2025-07-08 11:48:16 +0530

    Create missing GraphQL resolver files

---
+++ server/src/graphql/resolvers/GraphQLUpload.js
@@ -0,0 +1,6 @@
+// GraphQL Upload scalar type for handling file uploads
+// This is a CommonJS module to maintain compatibility with graphql-upload
+
+const GraphQLUpload = require('graphql-upload/GraphQLUpload.js');
+
+module.exports = GraphQLUpload;
